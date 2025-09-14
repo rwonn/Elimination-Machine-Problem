23f582b48ed0ff6c402856b781fd5c8d409b20ac
PixelDoo <pixdoo932@gmail.com>
2025-09-14 12:00:02 +0800

    Add files via upload
    manual math.abs, gawin ko pa mamaya na may sagot pa rin yung 0 coefficient

---
+++ Elimination-Machine-Problem/Elimination Machine Problem/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿using System;
+
+namespace Elimination_Machine_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA1 = new System.Windows.Forms.TextBox();
+            this.txtB1 = new System.Windows.Forms.TextBox();
+            this.txtA2 = new System.Windows.Forms.TextBox();
+            this.txtB2 = new System.Windows.Forms.TextBox();
+            this.txtC1 = new System.Windows.Forms.TextBox();
+            this.txtC2 = new System.Windows.Forms.TextBox();
+            this.cmbEliminate = new System.Windows.Forms.ComboBox();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtbSteps = new System.Windows.Forms.RichTextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtA1
+            // 
+            this.txtA1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA1.Location = new System.Drawing.Point(20, 51);
+            this.txtA1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtA1.Multiline = true;
+            this.txtA1.Name = "txtA1";
+            this.txtA1.Size = new System.Drawing.Size(132, 35);
+            this.txtA1.TabIndex = 0;
+            // 
+            // txtB1
+            // 
+            this.txtB1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB1.Location = new System.Drawing.Point(236, 51);
+            this.txtB1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtB1.Multiline = true;
+            this.txtB1.Name = "txtB1";
+            this.txtB1.Size = new System.Drawing.Size(132, 35);
+            this.txtB1.TabIndex = 1;
+            this.txtB1.TextChanged += new System.EventHandler(this.txtB1_TextChanged);
+            // 
+            // txtA2
+            // 
+            this.txtA2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA2.Location = new System.Drawing.Point(22, 136);
+            this.txtA2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtA2.Multiline = true;
+            this.txtA2.Name = "txtA2";
+            this.txtA2.Size = new System.Drawing.Size(132, 37);
+            this.txtA2.TabIndex = 2;
+            // 
+            // txtB2
+            // 
+            this.txtB2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB2.Location = new System.Drawing.Point(236, 136);
+            this.txtB2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtB2.Multiline = true;
+            this.txtB2.Name = "txtB2";
+            this.txtB2.Size = new System.Drawing.Size(132, 37);
+            this.txtB2.TabIndex = 3;
+            // 
+            // txtC1
+            // 
+            this.txtC1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC1.Location = new System.Drawing.Point(455, 51);
+            this.txtC1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtC1.Multiline = true;
+            this.txtC1.Name = "txtC1";
+            this.txtC1.Size = new System.Drawing.Size(132, 37);
+            this.txtC1.TabIndex = 4;
+            // 
+            // txtC2
+            // 
+            this.txtC2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC2.Location = new System.Drawing.Point(457, 136);
+            this.txtC2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtC2.Multiline = true;
+            this.txtC2.Name = "txtC2";
+            this.txtC2.Size = new System.Drawing.Size(132, 37);
+            this.txtC2.TabIndex = 5;
+            // 
+            // cmbEliminate
+            // 
+            this.cmbEliminate.AccessibleName = "";
+            this.cmbEliminate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbEliminate.FormattingEnabled = true;
+            this.cmbEliminate.Location = new System.Drawing.Point(20, 216);
+            this.cmbEliminate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbEliminate.Name = "cmbEliminate";
+            this.cmbEliminate.Size = new System.Drawing.Size(348, 28);
+            this.cmbEliminate.TabIndex = 6;
+            this.cmbEliminate.Text = "CHOOSE WHICH TO ELIMINATE FIRST";
+            this.cmbEliminate.SelectedIndexChanged += new System.EventHandler(this.cmbEliminate_SelectedIndexChanged_1);
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.Transparent;
+            this.btnSolve.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolve.Location = new System.Drawing.Point(22, 526);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(176, 60);
+            this.btnSolve.TabIndex = 7;
+            this.btnSolve.Text = "SOLVE";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Transparent;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(415, 526);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(174, 60);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // rtbSteps
+            // 
+            this.rtbSteps.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbSteps.Location = new System.Drawing.Point(616, 24);
+            this.rtbSteps.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rtbSteps.Name = "rtbSteps";
+            this.rtbSteps.Size = new System.Drawing.Size(577, 562);
+            this.rtbSteps.TabIndex = 10;
+            this.rtbSteps.Text = "";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(198, 431);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 16);
+            this.lblResult.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(162, 51);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(64, 35);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "x₁ +";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(165, 136);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(63, 37);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "x₁ +";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(385, 51);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(53, 33);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "x₂ =";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(387, 136);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(53, 37);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "x₂ =";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(1221, 611);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.rtbSteps);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.cmbEliminate);
+            this.Controls.Add(this.txtC2);
+            this.Controls.Add(this.txtC1);
+            this.Controls.Add(this.txtB2);
+            this.Controls.Add(this.txtA2);
+            this.Controls.Add(this.txtB1);
+            this.Controls.Add(this.txtA1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Elimination Method Solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    
+        private void cmbEliminate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // This event handler can be used to respond to selection changes if needed
+            // Currently no action required - selection will be checked when Solve is clicked
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA1;
+        private System.Windows.Forms.TextBox txtB1;
+        private System.Windows.Forms.TextBox txtA2;
+        private System.Windows.Forms.TextBox txtB2;
+        private System.Windows.Forms.TextBox txtC1;
+        private System.Windows.Forms.TextBox txtC2;
+        private System.Windows.Forms.ComboBox cmbEliminate;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtbSteps;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ Elimination-Machine-Problem/Elimination Machine Problem/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Elimination_Machine_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupComboBox(); // Ensure ComboBox is properly initialized
+            ConnectEventHandlers();
+        }
+
+        private void SetupComboBox()
+        {
+            // Clear any existing items and add our options
+            cmbEliminate.Items.Clear();
+            cmbEliminate.Items.Add("Eliminate x₁");
+            cmbEliminate.Items.Add("Eliminate x₂");
+            cmbEliminate.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void ConnectEventHandlers()
+        {
+            btnSolve.Click += btnSolve_Click;
+            btnClear.Click += btnClear_Click;
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get inputs
+                double a1 = double.Parse(txtA1.Text);
+                double b1 = double.Parse(txtB1.Text);
+                double c1 = double.Parse(txtC1.Text);
+
+                double a2 = double.Parse(txtA2.Text);
+                double b2 = double.Parse(txtB2.Text);
+                double c2 = double.Parse(txtC2.Text);
+
+                // Unicode subscripts
+                string x1s = "x₁"; // x₁
+                string x2s = "x₂"; // x₂
+
+                rtbSteps.Clear();
+                rtbSteps.AppendText("Step 1: Write the system of equations\n");
+                rtbSteps.AppendText(FormatEquation(a1, b1, c1, x1s, x2s) + "\n");
+                rtbSteps.AppendText(FormatEquation(a2, b2, c2, x1s, x2s) + "\n\n");
+
+                string choice = cmbEliminate.SelectedItem?.ToString();
+                if (string.IsNullOrEmpty(choice))
+                {
+                    lblResult.Text = "Please select which variable to eliminate.";
+                    return;
+                }
+
+                double factor1, factor2;
+                double A1, B1, C1, A2, B2, C2;
+
+                if (choice == "Eliminate x₁")
+                {
+                    // Find LCM of coefficients to eliminate x₁
+                    if (a1 == 0 || a2 == 0)
+                    {
+                        lblResult.Text = "Cannot eliminate x₁: one coefficient is zero.";
+                        return;
+                    }
+
+                    factor1 = MyAbs(a2);
+                    factor2 = MyAbs(a1);
+
+                    // Make sure we eliminate properly by using opposite signs
+                    if ((a1 > 0 && a2 > 0) || (a1 < 0 && a2 < 0))
+                    {
+                        factor2 = -factor2;
+                    }
+
+                    A1 = a1 * factor1; B1 = b1 * factor1; C1 = c1 * factor1;
+                    A2 = a2 * factor2; B2 = b2 * factor2; C2 = c2 * factor2;
+
+                    rtbSteps.AppendText($"Step 2: Multiply first equation by {factor1} and second by {factor2}\n");
+                    rtbSteps.AppendText(FormatEquation(A1, B1, C1, x1s, x2s) + "\n");
+                    rtbSteps.AppendText(FormatEquation(A2, B2, C2, x1s, x2s) + "\n\n");
+
+                    // Add equations to eliminate x₁
+                    double coeffX2 = B1 + B2;
+                    double constant = C1 + C2;
+
+                    rtbSteps.AppendText("Step 3: Add equations to eliminate " + x1s + "\n");
+                    if (coeffX2 == 1)
+                        rtbSteps.AppendText($"{x2s} = {constant}\n\n");
+                    else if (coeffX2 == -1)
+                        rtbSteps.AppendText($"-{x2s} = {constant}\n\n");
+                    else
+                        rtbSteps.AppendText($"{coeffX2}{x2s} = {constant}\n\n");
+
+                    if (coeffX2 == 0)
+                    {
+                        if (constant == 0)
+                            lblResult.Text = "Infinite solutions (dependent system).";
+                        else
+                            lblResult.Text = "No solution (inconsistent system).";
+                        return;
+                    }
+
+                    double x2 = constant / coeffX2;
+                    rtbSteps.AppendText($"Step 4: Solve for {x2s}\n");
+                    rtbSteps.AppendText($"{x2s} = {constant} ÷ {coeffX2} = {x2:F3}\n\n");
+
+                    // Substitute back to find x₁
+                    double x1 = (c1 - b1 * x2) / a1;
+                    rtbSteps.AppendText($"Step 5: Substitute {x2s} = {x2:F3} into first equation\n");
+                    rtbSteps.AppendText(FormatEquation(a1, b1, c1, x1s, x2s) + "\n");
+                    rtbSteps.AppendText($"{a1}{x1s} + {b1}({x2:F3}) = {c1}\n");
+                    rtbSteps.AppendText($"{a1}{x1s} = {c1} - {b1 * x2:F3}\n");
+                    rtbSteps.AppendText($"{x1s} = {(c1 - b1 * x2):F3} ÷ {a1} = {x1:F3}\n\n");
+
+                    lblResult.Text = $"Solution: {x1s} = {x1:F3}, {x2s} = {x2:F3}";
+                }
+                else if (choice == "Eliminate x₂")
+                {
+                    // Find LCM of coefficients to eliminate x₂
+                    if (b1 == 0 || b2 == 0)
+                    {
+                        lblResult.Text = "Cannot eliminate x₂: one coefficient is zero.";
+                        return;
+                    }
+
+                    factor1 = MyAbs(b2);
+                    factor2 = MyAbs(b1);
+
+                    // Make sure we eliminate properly by using opposite signs
+                    if ((b1 > 0 && b2 > 0) || (b1 < 0 && b2 < 0))
+                    {
+                        factor2 = -factor2;
+                    }
+
+                    A1 = a1 * factor1; B1 = b1 * factor1; C1 = c1 * factor1;
+                    A2 = a2 * factor2; B2 = b2 * factor2; C2 = c2 * factor2;
+
+                    rtbSteps.AppendText($"Step 2: Multiply first equation by {factor1} and second by {factor2}\n");
+                    rtbSteps.AppendText(FormatEquation(A1, B1, C1, x1s, x2s) + "\n");
+                    rtbSteps.AppendText(FormatEquation(A2, B2, C2, x1s, x2s) + "\n\n");
+
+                    // Add equations to eliminate x₂
+                    double coeffX1 = A1 + A2;
+                    double constant = C1 + C2;
+
+                    rtbSteps.AppendText("Step 3: Add equations to eliminate " + x2s + "\n");
+                    if (coeffX1 == 1)
+                        rtbSteps.AppendText($"{x1s} = {constant}\n\n");
+                    else if (coeffX1 == -1)
+                        rtbSteps.AppendText($"-{x1s} = {constant}\n\n");
+                    else
+                        rtbSteps.AppendText($"{coeffX1}{x1s} = {constant}\n\n");
+
+                    if (coeffX1 == 0)
+                    {
+                        if (constant == 0)
+                            lblResult.Text = "Infinite solutions (dependent system).";
+                        else
+                            lblResult.Text = "No solution (inconsistent system).";
+                        return;
+                    }
+
+                    double x1 = constant / coeffX1;
+                    rtbSteps.AppendText($"Step 4: Solve for {x1s}\n");
+                    rtbSteps.AppendText($"{x1s} = {constant} ÷ {coeffX1} = {x1:F3}\n\n");
+
+                    // Substitute back to find x₂
+                    double x2 = (c1 - a1 * x1) / b1;
+                    rtbSteps.AppendText($"Step 5: Substitute {x1s} = {x1:F3} into first equation\n");
+                    rtbSteps.AppendText(FormatEquation(a1, b1, c1, x1s, x2s) + "\n");
+                    rtbSteps.AppendText($"{a1}({x1:F3}) + {b1}{x2s} = {c1}\n");
+                    rtbSteps.AppendText($"{b1}{x2s} = {c1} - {a1 * x1:F3}\n");
+                    rtbSteps.AppendText($"{x2s} = {(c1 - a1 * x1):F3} ÷ {b1} = {x2:F3}\n\n");
+
+                    lblResult.Text = $"Solution: {x1s} = {x1:F3}, {x2s} = {x2:F3}";
+                }
+            }
+            catch (FormatException)
+            {
+                lblResult.Text = "Invalid input. Please enter numeric values.";
+            }
+            catch (DivideByZeroException)
+            {
+                lblResult.Text = "Division by zero error. Check your coefficients.";
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clear all textboxes
+            txtA1.Clear();
+            txtB1.Clear();
+            txtC1.Clear();
+            txtA2.Clear();
+            txtB2.Clear();
+            txtC2.Clear();
+
+            // Clear steps, result, and selection
+            rtbSteps.Clear();
+            lblResult.Text = "";
+            cmbEliminate.SelectedIndex = -1;
+        }
+
+        /// <summary>
+        /// Formats an equation as a readable string like: 2x₁ + 3x₂ = 7
+        /// </summary>
+        /// 
+        private double MyAbs(double num)
+        {
+            if (num < 0)
+                return -num;
+            else
+                return num;
+        }
+        private string FormatEquation(double a, double b, double c, string x1s, string x2s)
+        {
+            string eq = "";
+
+            // x₁ term
+            if (a != 0)
+            {
+                if (a == 1) eq += x1s;
+                else if (a == -1) eq += $"-{x1s}";
+                else eq += $"{a}{x1s}";
+            }
+
+            // x₂ term
+            if (b != 0)
+            {
+                if (eq.Length > 0) // There's already a term
+                {
+                    if (b > 0) eq += " + ";
+                    else eq += " - ";
+
+                    double absB = MyAbs(b);
+                    if (absB == 1) eq += x2s;
+                    else eq += $"{absB}{x2s}";
+                }
+                else // This is the first term
+                {
+                    if (b == 1) eq += x2s;
+                    else if (b == -1) eq += $"-{x2s}";
+                    else eq += $"{b}{x2s}";
+                }
+            }
+
+            // If both a and b are 0
+            if (a == 0 && b == 0) eq = "0";
+
+            // Add RHS
+            eq += $" = {c}";
+            return eq;
+        }
+
+        private void txtB1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbEliminate_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSolve_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
